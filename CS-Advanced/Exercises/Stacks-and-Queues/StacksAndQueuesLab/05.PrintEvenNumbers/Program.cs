770f93a2e78e643ada37fa3d478920e86d826e71
bonevboyan <boyanbonev43@gmail.com>
2021-01-17 14:14:39 +0200

    Added Queue Problems Solutions

---
+++ CS-Advanced/Exercises/Stacks-and-Queues/StacksAndQueuesLab/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> nums = new Queue<int>(input);
+            Queue<int> evenNums = new Queue<int>();
+            while (nums.Count > 0)
+            {
+                if (nums.Peek() % 2 == 0)
+                {
+                    evenNums.Enqueue(nums.Dequeue());
+                }
+                else
+                {
+                    nums.Dequeue();
+                }
+            }
+            Console.WriteLine(string.Join(", ", evenNums));
+        }
+    }
+}
---
+++ CS-Advanced/Exercises/Stacks-and-Queues/StacksAndQueuesLab/06.Supermarket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _06.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                if(command == "Paid")
+                {
+                    while(queue.Count > 0)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(queue.Count + " people remaining.");
+        }
+    }
+}
