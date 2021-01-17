767352a828d663efecede911f533453cc4efce92
bonevboyan <boyanbonev43@gmail.com>
2021-01-17 18:35:23 +0200

    Added MD Array Problems Solutions

---
+++ CS-Advanced/Exercises/Multidimensional-Arrays/MultidimensionalArraysLab/04.SymbolInMatrix/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+            for (int row = 0; row < n; row++)
+            {
+                char[] rowData = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            char ch = char.Parse(Console.ReadLine());
+            int r = -1;
+            int c = -1;
+            bool isTrue = false;
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if(matrix[row,col] == ch)
+                    {
+                        isTrue = true;
+                        r = row;
+                        c = col;
+                        goto outer;
+                    }
+                }
+            }
+            outer:;
+            if (r != -1)
+            {
+                Console.WriteLine($"({r}, {c})");
+            }
+            else
+            {
+                Console.WriteLine($"{ch} does not occur in the matrix");
+            }
+        }
+    }
+}
---
+++ CS-Advanced/Exercises/Multidimensional-Arrays/MultidimensionalArraysLab/05.SquareWithMaximumSum/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.SquareWithMaximumSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = input[0];
+            int cols = input[1];
+
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                int[] rowData = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            int maxSum = 0;
+            int maxRow = 0;
+            int maxCol = 0; 
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    int squareSum = matrix[row, col] + matrix[row, col + 1] + matrix[row + 1, col + 1] + matrix[row + 1, col];
+                    if(squareSum > maxSum)
+                    {
+                        maxSum = squareSum;
+                        maxRow = row;
+                        maxCol = col;
+
+                    }
+                }
+            }
+
+            for (int row = maxRow; row < maxRow + 2; row++)
+            {
+                for (int col = maxCol; col < maxCol + 2; col++)
+                {
+                    Console.Write(matrix[row,col] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(maxSum);
+        }
+    }
+}
