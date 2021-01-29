404c964546d7a568c888c777307355b001a5addd
bonevboyan <boyanbonev43@gmail.com>
2021-01-29 18:09:40 +0200

    Streams and Files Lab Exercises

---
+++ CS-Advanced/Exercises/Streams-Files-Directories/StreamsFilesDirectoriesLab/01.OddLines/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace _01.OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+                string current = reader.ReadLine();
+                int row = 0;
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    while (current != null)
+                    {
+                        if (row % 2 == 1)
+                        {
+                            writer.WriteLine(current);
+                        }
+                        current = reader.ReadLine();
+                        row++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CS-Advanced/Exercises/Streams-Files-Directories/StreamsFilesDirectoriesLab/03.WordCount/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03.WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            using (StreamReader wordReader = new StreamReader("../../../words.txt"))
+            {
+                string[] wordsArr = wordReader.ReadToEnd().Split();
+                foreach (var word in wordsArr)
+                {
+                    words.Add(word, 0);
+                }
+                using (StreamReader textReader = new StreamReader("../../../Input.txt"))
+                {
+                    string[] text = textReader.ReadToEnd().Split().Select(o => o.Replace(",", "").Replace("-", "").Replace(".", "").Replace("!", "").Replace("?", "").ToLower()).ToArray();
+                    foreach (var word in text)
+                    {
+                        if (words.ContainsKey(word))
+                        {
+                            words[word]++;
+                        }
+                    }
+                }
+            }
+            using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+            {
+                foreach (var word in words.OrderByDescending(o => o.Value))
+                {
+                    writer.WriteLine($"{word.Key} - {word.Value}");
+                }
+            }
+            
+
+        }
+    }
+}
