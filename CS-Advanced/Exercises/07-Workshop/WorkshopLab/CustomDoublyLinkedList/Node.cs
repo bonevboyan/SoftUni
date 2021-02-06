27a3d69a1c04d6ae1a3a9236cc93636ef11ec09f
bonevboyan <boyanbonev43@gmail.com>
2021-02-06 16:33:19 +0200

    Linked List Implementation Workshop

---
+++ CS-Advanced/Exercises/07-Workshop/WorkshopLab/CustomDoublyLinkedList/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDoublyLinkedList
+{
+    class Node
+    {
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; set; }
+        public Node Next { get; set; }
+        public Node Previous { get; set; }
+    }
+
+}
---
+++ CS-Advanced/Exercises/07-Workshop/WorkshopLab/CustomDoublyLinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomDoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
