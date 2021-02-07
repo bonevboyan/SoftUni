9faa9adc7002f38ec4f76938ba4296ea5ca1b799
bonevboyan <boyanbonev43@gmail.com>
2021-02-07 13:05:39 +0200

    Added Generics Lab

---
+++ CS-Advanced/Exercises/08-Generics/GenericsLab/02.GenericArrayCreator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            int[] integers = ArrayCreator.Create(10, 33);
+
+        }
+    }
+}
