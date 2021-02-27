808fcc14378e6d41d268d57d4ed87e626a15e155
bonevboyan <boyanbonev43@gmail.com>
2021-02-27 21:08:02 +0200

    Added Interfaces Exercises

---
+++ CS-OOP/Exercises/01-Inheritance/InheritanceExercise/PlayersAndMonsters/Hero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        public Hero(string username, int level)
+        {
+            Level = level;
+            Username = username;
+        }
+        public int Level { get; set; }
+        public string Username { get; set; }
+        public override string ToString()
+        {
+            return $"Type: {GetType().Name} Username: {Username} Level: {Level}";
+        }
+    }
+}
