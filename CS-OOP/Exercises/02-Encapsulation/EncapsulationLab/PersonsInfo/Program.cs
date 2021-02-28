b124144c32a3eee5ff2d1edd5d437ab6e65fec3a
bonevboyan <boyanbonev43@gmail.com>
2021-02-28 12:30:34 +0200

    Added Encapsulation Lab

---
+++ CS-OOP/Exercises/02-Encapsulation/EncapsulationLab/PersonsInfo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Team team = new Team("SoftUni");
+
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split();
+                try
+                {
+                    Person person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]), decimal.Parse(cmdArgs[3]));
+                    team.AddPlayer(person);
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+
+            Console.WriteLine(team);
+        }
+    }
+}
+
+
+
---
+++ CS-OOP/Exercises/02-Encapsulation/EncapsulationLab/PersonsInfo/Team.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+        public IReadOnlyList<Person> FirstTeam { get { return firstTeam.AsReadOnly(); } }
+        public IReadOnlyList<Person> ReserveTeam { get { return reserveTeam.AsReadOnly(); } }
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+        public override string ToString()
+        {
+            return $"First team has {firstTeam.Count} players." + Environment.NewLine +
+                   $"Reserve team has {reserveTeam.Count} players.";
+        }
+
+    }
+}
