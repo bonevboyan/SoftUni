c4e2dd03b27e790550c28a7b99af0a60d03b435b
bonevboyan <boyanbonev43@gmail.com>
2021-03-06 20:01:04 +0200

    Added Polymorphism Lab

---
+++ CS-OOP/Exercises/04-Polymorphism/PolymorphismLab/Shapes/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius
+        {
+            get { return this.radius; }
+            set { this.radius = value; }
+        }
+
+        public override double CalculateArea()
+        {
+            return Radius * Radius * Math.PI;
+        }
+
+        public override double CalculatePerimeter()
+        {
+
+            return Radius * 2 * Math.PI;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
---
+++ CS-OOP/Exercises/04-Polymorphism/PolymorphismLab/Shapes/Rectangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Width
+        {
+            get { return this.width; }
+            set { this.width = value; }
+        }
+        public double Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+
+        public override double CalculateArea()
+        {
+            return Height * Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Height + 2 * Width;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
