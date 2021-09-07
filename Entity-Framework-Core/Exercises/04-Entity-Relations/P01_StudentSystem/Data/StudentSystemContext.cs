18bea9c35a762d14f889af385942b60c86f892f7
bonevboyan <boyanbonev43@gmail.com>
2021-09-08 00:28:47 +0300

    Added Entity Relations Exercise 1. Student System

---
+++ Entity-Framework-Core/Exercises/04-Entity-Relations/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+
+        public StudentSystemContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+
+        public DbSet<Resource> Resources { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=StudentSystem;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(x => new { x.CourseId, x.StudentId });
+
+            modelBuilder.Entity<Course>()
+                .Property(c => c.Description)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Resource>()
+                .Property(r => r.Url)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Homework>()
+                .Property(h => h.Content)
+                .IsUnicode(false);
+        }
+    }
+}
