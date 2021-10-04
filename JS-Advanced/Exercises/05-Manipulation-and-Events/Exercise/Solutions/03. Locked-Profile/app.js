e48f4f3d94346b04308fcb93e74d1e654a6bc66b
bonevboyan <boyanbonev43@gmail.com>
2021-10-04 22:57:01 +0300

    Added Solution to Problem 3

--- JS-Advanced/Exercises/05-Manipulation-and-Events/Exercise/Solutions/03. Locked-Profile/app.js
+++ JS-Advanced/Exercises/05-Manipulation-and-Events/Exercise/Solutions/03. Locked-Profile/app.js
@@ -1,3 +1,15 @@
 function lockedProfile() {
-    console.log('TODO...')
+    Array.from(document.getElementsByTagName("button")).forEach(x => {
+        x.addEventListener("click", onClick);
+    });
+
+    function onClick(ev){
+        const children = ev.target.parentElement.children;
+        if(Array.from(document.getElementsByName(children[2].name)).filter(x => x.value == "unlock")[0].checked){
+            let element = Array.from(document.querySelectorAll('input[type="email"]'))
+                .find(x => x.parentElement.id == children[9].id).parentElement;
+            element.style.display = element.style.display == "block" ? "none" : "block";
+            ev.target.textContent =  ev.target.textContent == "Show more" ? "Hide it" : "Show more";
+        }
+    }
 }
