bae872677e31bf94256ad48991832ae7527b3fff
bonevboyan <boyanbonev43@gmail.com>
2021-10-06 17:12:59 +0300

    Added Solutions to Problems 4 and 5

--- JS-Advanced/Exercises/05-Manipulation-and-Events/Exercise/Solutions/04. Fill-Dropdown/dropdown.js
+++ JS-Advanced/Exercises/05-Manipulation-and-Events/Exercise/Solutions/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    let text = document.getElementById("newItemText");
+    let value = document.getElementById("newItemValue");
+
+    let option = document.createElement("option");
+    option.text = text.value;
+    option.value = value.value;
+    
+    document.getElementById("menu").appendChild(option);
+
+    text.value = "";
+    value.value = "";
 }
--- JS-Advanced/Exercises/05-Manipulation-and-Events/Exercise/Solutions/05. Encode-and-Decode-Messages/app.js
+++ JS-Advanced/Exercises/05-Manipulation-and-Events/Exercise/Solutions/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,23 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let buttons = Array.from(document.getElementsByTagName("button"));
+    buttons[0].addEventListener("click", onEncode);
+    buttons[1].addEventListener("click", onDecode);
+
+    function onEncode(ev) {
+        let textAreas = document.getElementsByTagName("textarea");
+        let encodedMessage = textAreas[0].value.split("").map(x => String.fromCharCode(x.charCodeAt(0) + 1)).join("");
+
+        textAreas[0].value = "";
+
+        textAreas[1].value = encodedMessage;
+    }
+
+    function onDecode(ev) {
+        let textAreas = document.getElementsByTagName("textarea");
+        let encodedMessage = textAreas[1].value.split("").map(x => String.fromCharCode(x.charCodeAt(0) - 1)).join("");
+
+        textAreas[1].value = "";
+
+        textAreas[1].value = encodedMessage;
+    }
 }
