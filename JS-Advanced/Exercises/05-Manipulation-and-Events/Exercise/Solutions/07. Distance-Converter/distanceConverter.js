e89176c16246d92a281870d50383f7a47950ed14
bonevboyan <boyanbonev43@gmail.com>
2021-10-08 12:32:50 +0300

    Added Solutions to Bonus DOM Events Exercises

--- JS-Advanced/Exercises/05-Manipulation-and-Events/Exercise/Solutions/07. Distance-Converter/distanceConverter.js
+++ JS-Advanced/Exercises/05-Manipulation-and-Events/Exercise/Solutions/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,25 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    document.getElementById("convert").addEventListener("click", onClick);
+
+    function onClick(ev){
+        let inputValue = Number(document.getElementById("inputDistance").value);
+        let inputUnit = document.getElementById("inputUnits").value;
+
+        const unitConverter = {
+            km: 1000,
+            m: 1,
+            cm: 0.01,
+            mm: 0.001,
+            mi: 1609.34,
+            yrd: 0.9144,
+            ft: 0.3048,
+            in: 0.0254
+        }
+
+        let valueInMeters = inputValue * unitConverter[inputUnit];
+
+        let outputUnit = document.getElementById("outputUnits").value;
+
+        document.getElementById("outputDistance").value = valueInMeters / unitConverter[outputUnit];
+    }
 }
--- JS-Advanced/Exercises/05-Manipulation-and-Events/Exercise/Solutions/08. Sudomu/solution.js
+++ JS-Advanced/Exercises/05-Manipulation-and-Events/Exercise/Solutions/08. Sudomu/solution.js
@@ -1,4 +1,58 @@
 function solve() {
+    document.getElementsByTagName("button")[0].addEventListener("click", onCheck);
 
-    //TODO...
+    document.getElementsByTagName("button")[1].addEventListener("click", onClear);
+
+    let htmlTable = document.querySelector("table tbody");
+
+    let outputElement = document.getElementById("check").children[0];
+
+    function onCheck(ev){
+        let myTable = [];
+        
+        for (i = 0; i < htmlTable.rows.length; i++) {
+            
+            let objCells = htmlTable.rows.item(i).cells;
+            let tableRow = [];
+            
+            for (let j = 0; j < objCells.length; j++) {
+                tableRow.push(Number(objCells.item(j).children[0].value));
+            }
+            myTable.push(tableRow);
+        }
+
+        if(sudokuIsSolved(myTable)){
+            htmlTable.parentElement.style.border = "2px solid green";
+            outputElement.textContent = "You solve it! Congratulations!";
+            outputElement.style.color = "green";
+        }else{
+            htmlTable.parentElement.style.border = "2px solid red";
+            outputElement.textContent = "NOP! You are not done yet...";
+            outputElement.style.color = "red";
+        }
+    }
+    function onClear(ev){
+        for (i = 0; i < htmlTable.rows.length; i++) {
+            let objCells = htmlTable.rows.item(i).cells;
+            for (let j = 0; j < objCells.length; j++) {
+                objCells.item(j).children[0].value = "";
+            }
+        }
+        htmlTable.parentElement.style.border = "";
+        outputElement.textContent = "";
+    }
+    function sudokuIsSolved(array) {
+        let f = true;
+        for (let index = 0; index < array.length; index++) {
+            if((new Set(array[index])).size !== array[index].length){
+                f = false;
+            }
+        }
+        for (let index = 0; index < array.length; index++) {
+            if(array[0][index] == array[1][index] == array[2][index]){
+                f = false;
+            }
+        }
+        return f;
+    }
 }
