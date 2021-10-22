6441cc9edf545e9da08cf6f394776186c4e2f8c8
bonevboyan <boyanbonev43@gmail.com>
2021-10-23 00:09:25 +0300

    Finished PetMe Solution

--- JS-Advanced/Exams/Exam-Prep/Solutions/01.PetMe/solution.js
+++ JS-Advanced/Exams/Exam-Prep/Solutions/01.PetMe/solution.js
@@ -4,14 +4,21 @@ function solve() {
     button.addEventListener("click", () => {
         
         let [name, age, kind, owner] = input.map(x => x.value);
+
+        if(!name || !kind || !owner || !age || isNaN(Number(age))){
+            return;
+        }
+        
         for (const token of input) {
             token.value = "";
         }
 
         let newDog = document.createElement("li");
 
-        newDog.innerHTML = `<p><strong>${name}</strong> is a <strong>${age}</strong> year old <strong>${kind}</strong></p>
-                            <span>Owner: ${owner}</span>`;
+        newDog.innerHTML = `<p><strong>${name}</strong> is a <strong>${age}</strong> year old <strong>${kind}</strong></p>`;
+        let oldOwnerSpan = document.createElement("span");
+        oldOwnerSpan.textContent = `Owner: ${owner}`;
+        newDog.appendChild(oldOwnerSpan);
 
         let contactButton = document.createElement("button");
         contactButton.textContent = "Contact with owner";
@@ -34,14 +41,21 @@ function solve() {
             newDog.appendChild(inputDiv);
 
             takeButton.addEventListener("click", () => {
+                let newOwner = inputDiv.children[0].value;
+
+                if(!newOwner){
+                    return;
+                }
+
                 forAdoption.removeChild(newDog);
                 adopted.appendChild(newDog);
 
                 newDog.removeChild(inputDiv);
-                //newDog.removeChild(takeButton);
+
+                newDog.removeChild(oldOwnerSpan);
 
                 let ownerSpan = document.createElement("span");
-                ownerSpan.textContent = `New Owner: ${inputDiv.children[0].value}`;
+                ownerSpan.textContent = `New Owner: ${newOwner}`;
                 
                 let checkedButton = document.createElement("button");
                 checkedButton.textContent = "Checked";
@@ -49,7 +63,9 @@ function solve() {
                 newDog.appendChild(ownerSpan);
                 newDog.appendChild(checkedButton);
 
-                console.log(newDog);
+                checkedButton.addEventListener("click", () => {
+                    adopted.removeChild(newDog);
+                })
             })
         })
     })
