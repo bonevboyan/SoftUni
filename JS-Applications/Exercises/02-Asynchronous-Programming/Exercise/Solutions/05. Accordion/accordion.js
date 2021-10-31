dc66be45971ed6370879fac8119d68481dd4dfd3
bonevboyan <boyanbonev43@gmail.com>
2021-11-01 01:11:58 +0200

    Finished Asynchronous Programming Exercises

--- JS-Applications/Exercises/02-Asynchronous-Programming/Exercise/Solutions/05. Accordion/accordion.js
+++ JS-Applications/Exercises/02-Asynchronous-Programming/Exercise/Solutions/05. Accordion/accordion.js
@@ -1,30 +1,53 @@
 function solution() {
+    getArticles();
     async function getArticles() {
-        let responce = await fetch("http://localhost:3030/jsonstore/advanced/articles/list");
-
-        if(responce.ok == false){
-            throw new Error(`${response.status} ${response.statusText}`);
-        }
-
-        let articles = await responce.json();
-
-        let articleStructure = document.querySelector(".accordion");
-
-        for (const article of articles) {
-            responce = await fetch(`http://localhost:3030/jsonstore/advanced/articles/details/${article._id}`);
-            if(responce.ok == false){
+        try {
+            let responce = await fetch("http://localhost:3030/jsonstore/advanced/articles/list");
+    
+            if (responce.ok == false) {
                 throw new Error(`${response.status} ${response.statusText}`);
             }
-            let info = responce.json();
+    
+            let articles = await responce.json();
+    
+            let articleStructure = document.querySelector(".accordion");
+    
+            for (const article of articles) {
+                responce = await fetch(`http://localhost:3030/jsonstore/advanced/articles/details/${article._id}`);
+                if (responce.ok == false) {
+                    throw new Error(`${response.status} ${response.statusText}`);
+                }
+                let info = await responce.json();
+    
+                let newArticle = document.createElement("div");
+                newArticle.classList.add("accordion");
+    
+                newArticle.innerHTML = articleStructure.innerHTML;
+    
+                newArticle.querySelector(".head span").textContent = info.title;
+                newArticle.querySelector(".head button").id = info._id;
+                newArticle.querySelector(".head button").addEventListener("click", onClick);
+                newArticle.querySelector(".extra p").textContent = info.content;
+                newArticle.querySelector(".extra").style.display = 'none'
+    
+                articleStructure.parentElement.appendChild(newArticle);
+            }
+    
+            articleStructure.parentElement.removeChild(articleStructure);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
 
-            let newArticle = document.createElement("div");
-            newArticle.classList.add("accordion");
+    function onClick(e) {
+        e.target.textContent = e.target.textContent == "More" ? "Less" : "More";
 
-            newArticle.innerHTML = articleStructure.innerHTML;
+        let extra = e.target.parentElement.parentElement.querySelector(".extra");
 
-            newArticle.querySelector(".head span").textContent = article.title;
-            newArticle.querySelector(".head button").id = article._id;
-            newArticle.querySelector(".extra p").textContent = article.content;
+        if (extra.style.display === 'none') {
+            extra.style.display = 'block';
+        } else {
+            extra.style.display = 'none'
         }
     }
 }
--- JS-Applications/Exercises/02-Asynchronous-Programming/Exercise/Solutions/06.Blog/app.js
+++ JS-Applications/Exercises/02-Asynchronous-Programming/Exercise/Solutions/06.Blog/app.js
@@ -1,5 +1,71 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById("btnLoadPosts").addEventListener("click", onLoad);
+    document.getElementById("btnViewPost").addEventListener("click", onView);
+
+    async function onLoad(e) {
+        try {
+            let response = await fetch("http://localhost:3030/jsonstore/blog/posts");
+    
+            if (response.ok == false) {
+                throw new Error(`${response.status} ${response.statusText}`);
+            }
+    
+            let posts = await response.json();
+    
+            let select = document.getElementById("posts");
+    
+            for (const key in posts) {
+                let option = document.createElement("option");
+                option.setAttribute("value", key);
+    
+                option.textContent = posts[key].title;
+    
+                select.appendChild(option);
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    async function onView(e) {
+        try {
+            let postId = document.getElementById("posts").value.value;
+
+            let response = await fetch(`http://localhost:3030/jsonstore/blog/comments/${postId}`);
+    
+            if (response.ok == false) {
+                throw new Error(`${response.status} ${response.statusText}`);
+            }
+    
+            let comments = await response.json();
+    
+            let postComments = document.getElementById("post-comments");
+    
+            for (const comment in comments) {
+                let option = document.createElement("li");
+                option.setAttribute("id", key);
+    
+                option.textContent = comments[key].text;
+    
+                postComments.appendChild(option);
+            }
+
+            response = await fetch(`http://localhost:3030/jsonstore/blog/posts/${postId}`);
+    
+            if (response.ok == false) {
+                throw new Error(`${response.status} ${response.statusText}`);
+            }
+    
+            let post = await response.json();
+
+            document.getElementById("post-title").textContent = post.title;
+            document.getElementById("post-body").textContent = post.body;
+
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
 }
 
 attachEvents();
