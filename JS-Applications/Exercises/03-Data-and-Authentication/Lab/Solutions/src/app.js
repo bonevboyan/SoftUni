b034aa00a0665b0ae099c00659cd7101e47c3958
bonevboyan <boyanbonev43@gmail.com>
2021-11-06 18:32:01 +0200

    Added Data and Authentication Lab

--- JS-Applications/Exercises/03-Data-and-Authentication/Lab/Solutions/src/app.js
+++ JS-Applications/Exercises/03-Data-and-Authentication/Lab/Solutions/src/app.js
@@ -1,12 +1,12 @@
 async function getRecipes() {
-    const response = await fetch('http://localhost:3030/jsonstore/cookbook/recipes');
+    const response = await fetch('http://localhost:3030/data/recipes');
     const recipes = await response.json();
 
-    return Object.values(recipes);
+    return recipes;
 }
 
 async function getRecipeById(id) {
-    const response = await fetch('http://localhost:3030/jsonstore/cookbook/details/' + id);
+    const response = await fetch('http://localhost:3030/data/recipes/' + id);
     const recipe = await response.json();
 
     return recipe;
@@ -46,7 +46,29 @@ function createRecipeCard(recipe) {
     return result;
 }
 
+async function logout() {
+    const response = await fetch('http://localhost:3030/users/logout', {
+        method: 'get',
+        headers: {
+            'X-Authorization': sessionStorage.getItem('authToken')
+        },
+    });
+    if (response.status == 200) {
+        sessionStorage.removeItem('authToken');
+        window.location.pathname = 'index.html';
+    } else {
+        console.error(await response.json());
+    }
+}
+
 window.addEventListener('load', async () => {
+    if (sessionStorage.getItem('authToken') != null) {
+        document.getElementById('user').style.display = 'inline-block';
+        document.getElementById('logoutBtn').addEventListener('click', logout);
+    } else {
+        document.getElementById('guest').style.display = 'inline-block';
+    }
+
     const main = document.querySelector('main');
 
     const recipes = await getRecipes();
