d3dcb8e806bf71775140fa7addc3150592e5d5f0
bonevboyan <boyanbonev43@gmail.com>
2021-12-03 23:56:36 +0200

    Started Second Exam Prep

---
+++ Entity-Framework-Core/Exams/Exam-Prep-2/VaporStore/Data/Models/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VaporStore.Data.Models
+{
+    public class Game
+    {
+        public Game()
+        {
+            Purchases = new HashSet<Purchase>();
+            GameTags = new HashSet<GameTag>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public int DeveloperId { get; set; }
+
+        public Developer Developer { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public ICollection<Purchase> Purchases { get; set; }
+
+        public ICollection<GameTag> GameTags { get; set; }
+    }
+}
---
+++ Entity-Framework-Core/Exams/Exam-Prep-2/VaporStore/Data/Models/GameTag.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaporStore.Data.Models
+{
+    public class GameTag
+    {
+    }
+}
