98c60774c514e9185b4ec53229a76ccad1891193
bonevboyan <boyanbonev43@gmail.com>
2022-01-09 16:21:07 +0200

    Started Async Processing Lab

---
+++ CS-Web-Basics/02-Asynchronous-Processing/01.Chronometer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _01.Chronometer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chronometer c = new Chronometer();
+
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "start":
+                        c.Start();
+                        break;
+                    case "stop":
+                        c.Stop();
+                        break;
+                    case "lap":
+                        Console.WriteLine(c.Lap());
+                        break;
+                    case "laps":
+                        Console.WriteLine("Laps:\n" + (c.Laps.Count == 0 ? "no laps":  string.Join("\n", c.Laps)));
+                        break;
+                    case "time":
+                        Console.WriteLine(c.GetTime);
+                        break;
+                    case "reset":
+                        c.Reset();
+                        break;
+                    case "exit":
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+            }
+        }
+    }
+}
